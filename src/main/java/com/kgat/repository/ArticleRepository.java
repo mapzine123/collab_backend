d65e433525b9c3d1bbb70d247a14c96578a17c55
un <mapzine123@naver.com>
2024-08-15 02:32:55 +0900

    #8 - ArticleRepository 작성

---
+++ src/main/java/com/kgat/repository/ArticleRepository.java
@@ -0,0 +1,9 @@
+package com.kgat.repository;
+
+import com.kgat.entity.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArticleRepository extends JpaRepository<Article, Integer> {
+}
