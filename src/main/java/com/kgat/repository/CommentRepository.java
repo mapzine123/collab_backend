310c9c4c0b945dab93c07ef32144cdf1ad209a72
un <mapzine123@naver.com>
2024-08-21 22:45:54 +0900

    #16 - CommentRepository 작성
    ArticleNum에 대한 모든 댓글 가져오는 쿼리 메서드

---
+++ src/main/java/com/kgat/repository/CommentRepository.java
@@ -0,0 +1,13 @@
+package com.kgat.repository;
+
+import com.kgat.entity.Comment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    Page<Comment> findAllByArticleNum(Long articleNum, Pageable pageable);
+
+}
