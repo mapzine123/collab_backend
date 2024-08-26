e63961e2241735e71fcf2de6e46ab848200af002
un <mapzine123@naver.com>
2024-08-27 02:51:45 +0900

    #16 - CommentService 댓글 수정 기능 추가

--- src/main/java/com/kgat/service/CommentService.java
+++ src/main/java/com/kgat/service/CommentService.java
@@ -2,12 +2,16 @@ package com.kgat.service;
 
 import com.kgat.entity.Comment;
 import com.kgat.repository.CommentRepository;
+import com.kgat.vo.CommentData;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class CommentService {
@@ -26,5 +30,21 @@ public class CommentService {
 
     public Comment saveComment(Comment comment) {
         return commentRepository.save(comment);
+    }
+
+    @Transactional
+    public Comment updateComment(CommentData data) {
+        try {
+            commentRepository.updateComment(data.getCommentText(), data.getCommentId());
+
+            Optional<Comment> comment = commentRepository.findById(data.getCommentId());
+            System.out.println(comment);
+            return comment.orElse(null);
+        } catch(Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+
+
     }
 }
