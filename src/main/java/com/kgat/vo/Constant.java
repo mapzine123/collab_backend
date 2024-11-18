1b1e001546f8e507658fa08f86945fa8b4e65a3c
un <mapzine123@naver.com>
2024-11-18 23:05:00 +0900

    #23 - 상수 final 적용

--- src/main/java/com/kgat/vo/Constant.java
+++
@@ -1,5 +0,0 @@
-package com.kgat.vo;
-
-public abstract class Constant {
-    public static String PROFILE_PASS = "D:\\DEV\\resource\\profileImage\\";
-}
---
+++ src/main/java/com/kgat/vo/Constants.java
@@ -0,0 +1,5 @@
+package com.kgat.vo;
+
+public abstract class Constants {
+    public static final String PROFILE_PASS = "D:\\DEV\\resource\\profileImage\\";
+}
