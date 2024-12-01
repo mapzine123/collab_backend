aa30d3df5c05d3b0144fe15fd22ca08d7fca3004
un <mapzine123@naver.com>
2024-12-02 02:16:30 +0900

    Test : 채팅방에 메시지를 보내면 같은 채팅방의 모든 세션이 메시지를 받을 수 있다 [RED]

--- src/test/java/com/kgat/websocket/ChatHandlerTest.java
+++ src/test/java/com/kgat/websocket/ChatHandlerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 
 import java.net.URI;
@@ -22,6 +23,10 @@ import static org.mockito.Mockito.*;
     - Mock 객체들을 주입받을 실제 객체 생성
     - 테스트 대상이 되는 클래스에 사용
     - 이건 진짜야, 근대 가짜 객체들을 주입받아서 사용할거야.
+
+    stubbing
+    - 테스트에서 실제 객체 대신 가짜 객체를 사용할 때,
+      그 가짜 객체가 어떻게 동작해야 하는지 정의하는 것
 
  */
 
@@ -73,5 +78,53 @@ class ChatHandlerTest {
         // then
         assertThat(chatHandler.getSession("session1")).isNull();
         assertThat(chatHandler.getRoomSessions("uuid")).doesNotContain(session);
+    }
+
+    @Test
+    @DisplayName("채팅방에 메시지를 보내면 같은 채팅방의 모든 세션이 메시지를 받는다")
+    void sendMessageTest() throws Exception {
+        //given
+        // 채팅방 "uuid"에 있는 두 개의 세션 설정
+        WebSocketSession sender = mock(WebSocketSession.class);
+        WebSocketSession receiver = mock(WebSocketSession.class);
+
+        when(sender.getId()).thenReturn("session1");
+        when(receiver.getId()).thenReturn("session2");
+
+        // roomId를 uuid로 설정
+        URI uri = new URI("/chat/uuid");
+        when(sender.getUri()).thenReturn(uri);
+        when(receiver.getUri()).thenReturn(uri);
+
+        // 세션이 열려있다고 설정
+        when(sender.isOpen()).thenReturn(true);
+        when(receiver.isOpen()).thenReturn(true);
+
+        // 수신자, 발신자 세션 등록
+        ChatHandler chatHandler = new ChatHandler();
+        chatHandler.afterConnectionEstablished(sender);
+        chatHandler.afterConnectionEstablished(receiver);
+
+        TextMessage chatMessage = new TextMessage("안녕하세요");
+
+        // when
+        chatHandler.handleTextMessage(sender, chatMessage);
+
+        // then
+        /*
+            verify(sender)
+            - sender(mock객체)의 메서드 호출을 검증
+            - "이 mock 객체의 특정 메서드가 호출되었는지 확인하겠다"는 의미
+            
+            sendMessage()
+            - 검증하고자 하는 메서드
+            
+            any(TextMessage.class)
+            - 파라미터의 구체적인 값은 상관없이, TextMessage 타입이기만 하면 됨
+            
+         */
+        verify(sender).sendMessage(any(TextMessage.class));
+        verify(receiver).sendMessage(any(TextMessage.class));
+
     }
 }
