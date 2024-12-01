06772750aa7bc2f7c6ff11e05c4bb89f385a3514
un <mapzine123@naver.com>
2024-12-01 22:39:41 +0900

    Test : 웹 소켓 연결 테스트 [RED]

--- src/test/java/com/kgat/websocket/ChatHandlerTest.java
+++ src/test/java/com/kgat/websocket/ChatHandlerTest.java
@@ -1,19 +1,12 @@
 package com.kgat.websocket;
 
-import com.kgat.service.ChatService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
 
 /*
     @Mock
@@ -30,33 +23,22 @@ import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class ChatHandlerTest {
-    @Mock
-    private WebSocketSession webSocketSession;
-
-    @Mock
-    private ChatService chatService;
-
-    @InjectMocks
-    private ChatHandler chatHandler;
 
     @Test
-    @DisplayName("웹 소켓으로 메시지를 받으면 채팅방의 모든 참여자에게 메시지가 전송된다.")
-    void handleChatMessageTest() throws Exception {
-        // given : 채팅 JSON
-        String messageJson = """
-            {
-                "type": "CHAT",
-                "roomID": "room1",
-                "senderID": "user1",
-                "content": "안녕하세요"    
-            }
-            """;
-        TextMessage textMessage = new TextMessage(messageJson);
+    @DisplayName("웹 소켓이 연결되면 세션이 저장된다")
+    void connectionTest() throws Exception {
+        // given
+        WebSocketSession session = mock(WebSocketSession.class);
+        ChatHandler chatHandler = new ChatHandler();
+
+        // mock 설정
+        when(session.getId()).thenReturn("session1");
 
         // when
-        chatHandler.handleTextMessage(webSocketSession, textMessage);
+        chatHandler.afterConnectionEstablished(session);
 
         // then
-        verify(webSocketSession, times(1)).sendMessage(any(TextMessage.class))
+        assertThat(chatHandler.getSession("session1")).isEqualTo(session);
+
     }
 }
