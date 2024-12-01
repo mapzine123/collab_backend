c5e955a9e4c3a30b79c90d0cac34cc4ab29b4827
un <mapzine123@naver.com>
2024-12-01 22:40:07 +0900

    Test : 웹 소켓 연결 테스트 [GREEN]

---
+++ src/main/java/com/kgat/websocket/ChatHandler.java
@@ -0,0 +1,28 @@
+package com.kgat.websocket;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+public class ChatHandler extends TextWebSocketHandler {
+    private final Map<String, WebSocketSession> sessions = new ConcurrentHashMap<>();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessions.put(session.getId(), session);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        sessions.remove(session.getId());
+    }
+
+    public WebSocketSession getSession(String id) {
+        return sessions.get(id);
+    }
+}
