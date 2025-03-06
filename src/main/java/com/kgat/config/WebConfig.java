c24b2a99b83a9134a12634bb76f30b68e8d423e6
un <mapzine123@naver.com>
2025-03-06 19:48:45 +0900

    fix: Database infomation hiding

--- src/main/java/com/kgat/config/WebConfig.java
+++ src/main/java/com/kgat/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.kgat.config;
 
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,14 +9,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig {
+    @Value("${ALLOWED_ORIGINS}")
+    private String allowedOrigins;
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
+                String[] origins = allowedOrigins.split(",");
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins(origins)  // 동적으로 allowedOrigins 설정
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
